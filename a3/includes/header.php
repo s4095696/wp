4b29e5c4da6f890880839c77fa6a7c93dbf83d46
jdivito <s4095696@student.rmit.edu.au>
2024-10-31 22:21:30 +1100

    Update header.php

--- a3/includes/header.php
+++ a3/includes/header.php
@@ -1,7 +1,7 @@
-<head>
+<header>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pets Victoria - Pet Adoption</title>
+    <title>Pets Victoria</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     
@@ -14,4 +14,4 @@
 
 
     <link rel="stylesheet" href="css/style.css">
-</head>
+</header>
