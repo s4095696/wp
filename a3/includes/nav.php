42b4c8358ba498053962b35d57e216f8a22b3151
jdivito <s4095696@student.rmit.edu.au>
2024-10-31 22:21:47 +1100

    Update nav.php

--- a3/includes/nav.php
+++ a3/includes/nav.php
@@ -1,19 +1,15 @@
-<?php
-require_once 'logout.php';
+<?php 
+session_start(); 
 ?>
 
-<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">
-        <img src="images/logo.png" alt="Pets Victoria Logo" width="30" height="30">
-        Pets Victoria
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="index.php">Pets Victoria</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
                 <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
@@ -22,30 +18,24 @@ require_once 'logout.php';
             <li class="nav-item">
                 <a class="nav-link" href="gallery.php">Gallery</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="register.php">Register</a>
-            </li>
-            <li class="nav-item">
-                <?php if (isset($_SESSION['username'])): ?>
-                    <a class="nav-link" href="#" onclick="logout()">Logout</a>
-                <?php else: ?>
-                    <a class="nav-link" href="login.php">Login</a>
-                <?php endif; ?>
-            </li>
+            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="add.php">Add</a> <!-- Show Add link if logged in -->
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php?action=logout">Logout</a>
+                </li>
+            <?php else: ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a> <!-- Show Login link if not logged in -->
+                </li>
+            <?php endif; ?>
         </ul>
+        <form class="d-flex" action="search.php" method="GET" style="margin-left:auto;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_term" required>
+            <button class="btn btn-outline-success" type="submit">
+                <span class="material-symbols-outlined">search</span>
+            </button>
+        </form>
     </div>
-    <form class="form-inline my-2 my-lg-0" action="search.php" method="GET">
-        <input class="form-control mr-sm-2" type="search" name="search_term" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-            <span class="material-symbols-outlined">search</span>
-        </button>
-    </form>
 </nav>
-
-<!-- JavaScript for the navbar -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="script.js"></script>
-
-</body>
-</html>
